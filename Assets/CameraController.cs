5f9e01539ad67858cb630887d8b98ec8555170f1
Zoroark <Jonathan@Jonathan-Walters-MacBook-Pro.local>
2016-05-10 19:03:12 -0600

    movement and auto stopping for the ship

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -8,11 +8,11 @@ public class CameraController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-//		offset = transform.position - player.transform.position;
+		offset = transform.position - player.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-//		transform.position = player.transform.position + offset;
+		transform.position = player.transform.position + offset;
 	}
 }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,23 +3,60 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float YSpeed;
+	public float YSpeedForward;
+	public float YSpeedBackward;
+	public float XSpeedRight;
+	public float XSpeedLeft;
 	public float RotationSpeed;
 	public Rigidbody2D shipMovement;
 
 	// Use this for initialization
 	void Start () {
 		shipMovement = GetComponent<Rigidbody2D>();
+		YSpeedForward = 3;
+		YSpeedBackward = -2;
+		XSpeedRight = 0.5F;
+		XSpeedLeft = -0.5F;
+		RotationSpeed = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float moveY = Input.GetAxis("Vertical");
-		float moveRotation = Input.GetAxis("Horizontal");
+		
+		Vector2 YMovement = new Vector2(0.0F, 1.0F);
+		Vector2 XMovement = new Vector2(1.0F, 0.0F);
 
-		Vector2 YMovement = new Vector2(0.0F, moveY);
+		//Forward and back control and stopping
+		if (Input.GetKey (KeyCode.W)) {
+			shipMovement.AddRelativeForce (YMovement * YSpeedForward);
+		} else if (Input.GetKey (KeyCode.S)) {
+			shipMovement.AddRelativeForce (YMovement * YSpeedBackward);
+		} else if (shipMovement.velocity.y > 0.0F) {
+			shipMovement.AddRelativeForce (YMovement * YSpeedBackward);
+		} else if (shipMovement.velocity.y < 0.0F) {
+			shipMovement.AddRelativeForce (YMovement * YSpeedForward);
+		}
 
-		shipMovement.AddForce(YMovement * YSpeed);
-		shipMovement.AddTorque(moveRotation * RotationSpeed);
+		//Side to Side control and stopping
+		if (Input.GetKey (KeyCode.Q)) {
+			shipMovement.AddRelativeForce (XMovement * XSpeedLeft);
+		} else if (Input.GetKey (KeyCode.E)) {
+			shipMovement.AddRelativeForce (XMovement * XSpeedRight);
+		} else if (shipMovement.velocity.x > 0.0F) {
+			shipMovement.AddRelativeForce (XMovement * XSpeedLeft);
+		} else if (shipMovement.velocity.x < 0.0F) {
+			shipMovement.AddRelativeForce (XMovement * XSpeedRight);
+		}
+
+		//Rotational Control and stopping
+		if (Input.GetKey (KeyCode.A)) {
+			shipMovement.AddTorque (RotationSpeed);
+		} else if (Input.GetKey (KeyCode.D)) {
+			shipMovement.AddTorque (-RotationSpeed);
+		} else if (shipMovement.angularVelocity > 0.0F) {
+			shipMovement.AddTorque (-RotationSpeed);
+		} else if (shipMovement.angularVelocity < 0.0F) {
+			shipMovement.AddTorque (RotationSpeed);
+		}
 	}
 }
